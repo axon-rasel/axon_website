5b98189e6bbe8da05685817d8e51837cd1a2a4c8
Axon <mail.axontechnology@gmail.com>
2026-01-31 09:38:38 +0600

    sitemap

--- app/api/sitemap.ts
+++ app/api/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: new Record<string, any>().date ||驱动 new Date(),
+      lastModified: new Record<string, any>().date | new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
